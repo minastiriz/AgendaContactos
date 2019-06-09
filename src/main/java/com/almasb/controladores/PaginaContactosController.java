47a0b8fb2ee490f299a3dd63d0bc8220ac3340e1
Minas <al286336@uji.es>
2019-06-09 16:46:26 +0200

    cosas

---
+++ src/main/java/com/almasb/controladores/PaginaContactosController.java
@@ -0,0 +1,12 @@
+package com.almasb.controladores;
+
+import javafx.fxml.FXML;
+
+public class PaginaContactosController {
+
+    @FXML
+    private void pulsarAnadirContacto(){
+        System.out.println("Hago algo");
+
+    }
+}
--- src/main/java/com/almasb/logica/FXApp.java
+++ src/main/java/com/almasb/logica/FXApp.java
@@ -3,16 +3,24 @@ package com.almasb.logica;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
 
 public class FXApp extends Application {
+
+    private Stage escenario;
     @Override
     public void start(Stage stage) throws Exception{
-        stage.setScene(new Scene(new Pane(), 800, 600));
-        stage.show();
-        System.out.println("Hola cara alfombra");
+        escenario=stage;
+        FXMLLoader loader = new FXMLLoader(FXApp.class.getResource("/../IGU/PaginaContactos.fxml"));
+        //loader.setLocation(getClass().getResource("../IGU/PaginaContactos.fxml"));
+        AnchorPane pane = loader.load();
+        Scene escena = new Scene(pane);
+        escenario.setScene(escena);
+        escenario.show();
 
     }
     public static void main(String[] args){

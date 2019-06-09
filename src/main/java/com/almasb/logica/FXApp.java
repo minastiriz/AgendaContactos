ecbe35ff9733998be62d58dd7d4c7d6fa9f6b34f
Minas <al286336@uji.es>
2019-06-10 00:05:21 +0200

    na

--- src/main/java/com/almasb/logica/FXApp.java
+++ src/main/java/com/almasb/logica/FXApp.java
@@ -11,9 +11,9 @@ public class FXApp extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("/view/PaginaContactos.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/paginaContactos.fxml"));
         Scene escenario = new Scene(root);
-        stage.setTitle("ventana");
+        stage.setTitle("Contactos");
         stage.setScene(escenario);
         stage.show();
 

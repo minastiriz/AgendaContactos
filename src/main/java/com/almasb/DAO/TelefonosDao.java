bba5696c6662b30487c493a6085a1cf680433618
romOlivo <al341918@uji.es>
2019-06-14 17:39:06 +0200

    TelefonoDao done

--- src/main/java/com/almasb/DAO/TelefonosDao.java
+++ src/main/java/com/almasb/DAO/TelefonosDao.java
@@ -1,13 +1,14 @@
 package com.almasb.DAO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import com.almasb.IGU.Telefono;
 import com.almasb.utils.JsonParser;
 import com.almasb.utils.Requester;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class TelefonosDao {
 	
@@ -32,7 +33,12 @@ public class TelefonosDao {
         // Método para comprobar si un teléfono existe
         // Recibirá como parametro el numero en cuestion
         // Devolvera un boolean indicando si existe o no
-        return false;
+    	for (Map<String, String> data: JsonParser.jsonToMapList(req.requestGet("telefono/all"))){
+    		if (data.get("numero").equals(numero)){
+    			return true;
+    		}
+    	}
+    	return false;
     }
 
     public boolean addTelefono(Telefono tlf) {
@@ -65,10 +71,23 @@ public class TelefonosDao {
         // Método para editar un telefono perteneciente a un contacto
         // Recibirá como parámetros el id del contacto, el antiguo numero y los nuevos datos
         // Devolverá un boolean dependiendo de si se ha modificado correctamente
+    	Telefono telefono = new Telefono();
+    	telefono.setEtiquetaTelefono(nuevaEtiqueta);
+    	telefono.setNumero(Integer.parseInt(nuevoNum));
+    	telefono.setId(id);
+    	borrarTelefono(id, antiguoNum);
+    	addTelefono(telefono);
         return true;
     }
 
     public Set<String> etiquetasTelefonosDisponibles() {
-    	return null;
+    	Set<String> s = new HashSet<String>();
+    	String l = req.requestGet("email/etq");
+    	l = l.substring(1, l.length()-1);
+    	l = l.replaceAll("\"", "");
+    	for (String etiqueta: l.split(",")){
+    		s.add(etiqueta);
+    	}
+    	return s;
     }
 }
